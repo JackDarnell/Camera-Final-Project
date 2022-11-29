9e7dcc2e787d252c4bda9957a47ac6150206a60d
Jack Darnell <darnelljack@icloud.com>
2022-11-29 17:21:19 -0500

    Initial Commit

---
+++ Camera Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera Project
+//
+//  Created by Jack Darnell on 11/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
