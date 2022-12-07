33c3c49ef1a17cc01b07fdacc2866e1fb67b90ee
Jagger Denhof <denhofja@mail.gvsu.edu>
2022-12-07 18:26:04 -0500

    Boilerplate added

---
+++ Camera Project/OutfitTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OutfitTableViewCell.swift
+//  Camera Project
+//
+//  Created by Jagger Denhof on 12/7/22.
+//
+
+import UIKit
+
+class OutfitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
