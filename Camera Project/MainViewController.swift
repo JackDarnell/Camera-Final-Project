2eb1f30da0a7e2f5c56b251bcde8d25eea20e57e
Jack Darnell <darnelljack@icloud.com>
2022-12-07 22:36:32 -0500

    GOT THE COLLECTION VIEW WORKINGGIT PUSHGIT PUSH

--- Camera Project/MainViewController.swift
+++ Camera Project/MainViewController.swift
@@ -3,7 +3,7 @@
 //  Camera Project
 //
 //  Created by Jack Darnell on 12/7/22.
-//
+//x
 
 import UIKit
 
@@ -11,12 +11,13 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var ImageCollectionView: UICollectionView!
     
-    let reuseIdentifier = "snapshotCell"
+    let reuseIdentifier = "ImageCell"
     let snapshots : [Snapshot] = [Snapshot(image: "")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.ImageCollectionView.dataSource = self
+        self.ImageCollectionView.delegate = self
         // Do any additional setup after loading the view.
     }
 }
@@ -29,7 +30,7 @@ extension MainViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1//snapshots.count
+        return snapshots.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -45,6 +46,7 @@ extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(indexPath.item + 1)
     }
+    
 }
 /*
 // MARK: - Navigation
